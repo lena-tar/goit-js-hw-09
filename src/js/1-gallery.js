073b9feab4ddde93613a1d8ba435181b4ef88094
lena-tar <taran9933@gmail.com>
2026-02-01 18:58:34 +0100

    initial com

---
+++ src/js/1-gallery.js
@@ -0,0 +1,11 @@
+// const proof = document.querySelector('.back-link');
+// proof.addEventListener('click', foo);
+// function foo(event) {
+//   event.preventDefault();
+//   if (event.target) {
+//     console.log('no');
+//   } else {
+//     console.log('ok');
+//   }
+// }
+// console.log('ok');
---
+++ src/js/2-form.js
@@ -0,0 +1,10 @@
+// const proof = document.querySelector('.back-link');
+// proof.addEventListener('click', foo);
+// function foo(event) {
+//   event.preventDefault();
+//   if (event.target) {
+//     console.log('no');
+//   } else {
+//     console.log('ok');
+//   }
+// }
